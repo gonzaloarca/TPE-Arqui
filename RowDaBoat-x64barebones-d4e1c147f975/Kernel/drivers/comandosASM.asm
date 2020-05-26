f89f02a8fc8e441272e271ecc7dd1ed3f7bcf57f
Manuel Rodriguez <manurodriguez@itba.edu.ar>
2020-05-26 12:54:22 -0300

    Falta agregar cual dato se encuentra entre la direccion de retorno y donde arranca a guardarsela estructura

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/drivers/comandosASM.asm
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/drivers/comandosASM.asm
@@ -5,27 +5,28 @@ getRegisters:
 	push rbp
 	mov rbp, rsp
 
-	mov QWORD[rsp + 32], rax
-	lea rax, [rsp + 16]			; son 16 registros de 64bits(8 bytes) cada uno
-	mov QWORD[rax + 24], rbx
-	mov QWORD[rax + 32], rcx
-	mov QWORD[rax + 40], rdx
-	mov QWORD[rax + 48], rsi
-	mov QWORD[rax + 56], rdi
+	mov QWORD[rbp + 32], rax	; Arranca en +32 pues antes hay 4 datos de 64b que son(en orden ascendente): rbp, canary, retorno y ????
+	lea rax, [rbp + 32]			; son 16 registros de 64bits(8 bytes) cada uno
+	mov QWORD[rax + 8], rbx
+	mov QWORD[rax + 16], rcx
+	mov QWORD[rax + 24], rdx
+	mov QWORD[rax + 32], rsi
+	mov QWORD[rax + 40], rdi
 	push rbx
 	mov rbx, rbp
 	add rbx, 24					; donde se encontraba antes de llamar a esta funcion
-	mov QWORD[rax + 64], rbx
-	mov QWORD[rax + 72], rbx	; antes de llamar a la funcion rsp = rbp
+	mov QWORD[rax + 48], rbx
+	mov QWORD[rax + 56], rbx	; antes de llamar a la funcion rsp = rbp
 	pop rbx
-	mov QWORD[rax + 80], r8
-	mov QWORD[rax + 88], r9
-	mov QWORD[rax + 96], r10
-	mov QWORD[rax + 104], r11
-	mov QWORD[rax + 112], r12
-	mov QWORD[rax + 120], r13
-	mov QWORD[rax + 128], r14
-	mov QWORD[rax + 136], r15
+	mov QWORD[rax + 64], r8
+	mov QWORD[rax + 72], r9
+	mov QWORD[rax + 80], r10
+	mov QWORD[rax + 88], r11
+	mov QWORD[rax + 96], r12
+	mov QWORD[rax + 104], r13
+	mov QWORD[rax + 112], r14
+	mov QWORD[rax + 120], r15
+
 
 	mov rsp, rbp
 	pop rbp
