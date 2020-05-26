5335064ff2bc602a06889187e5e57a4343e1d771
Manuel Rodriguez <manurodriguez@itba.edu.ar>
2020-05-26 11:37:10 -0300

    Arreglo de inforeg

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/drivers/comandosASM.asm
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/drivers/comandosASM.asm
@@ -5,27 +5,27 @@ getRegisters:
 	push rbp
 	mov rbp, rsp
 
-	mov QWORD[rsp + 32], rax	; PREGUNTAR POR QUE DEBO GUARDAR A PARTIR DE rsp + 32 si la estructura arranca en rsp +16
-	lea rax, [rsp + 16]		; son 16 registros de 64bits(8 bytes) cada uno
+	mov QWORD[rsp + 32], rax
+	lea rax, [rsp + 16]			; son 16 registros de 64bits(8 bytes) cada uno
 	mov QWORD[rax + 24], rbx
 	mov QWORD[rax + 32], rcx
 	mov QWORD[rax + 40], rdx
 	mov QWORD[rax + 48], rsi
+	mov QWORD[rax + 56], rdi
 	push rbx
 	mov rbx, rbp
 	add rbx, 24					; donde se encontraba antes de llamar a esta funcion
-	mov QWORD[rax + 56], rbx
-	mov QWORD[rax + 64], rbx	; antes de llamar a la funcion rsp = rbp
+	mov QWORD[rax + 64], rbx
+	mov QWORD[rax + 72], rbx	; antes de llamar a la funcion rsp = rbp
 	pop rbx
-	mov QWORD[rax + 72], r8
-	mov QWORD[rax + 80], r9
-	mov QWORD[rax + 88], r10
-	mov QWORD[rax + 96], r11
-	mov QWORD[rax + 104], r12
-	mov QWORD[rax + 112], r13
-	mov QWORD[rax + 120], r14
-	mov QWORD[rax + 128], r15
-
+	mov QWORD[rax + 80], r8
+	mov QWORD[rax + 88], r9
+	mov QWORD[rax + 96], r10
+	mov QWORD[rax + 104], r11
+	mov QWORD[rax + 112], r12
+	mov QWORD[rax + 120], r13
+	mov QWORD[rax + 128], r14
+	mov QWORD[rax + 136], r15
 
 	mov rsp, rbp
 	pop rbp
